0bfe2c73fe5335fbc4750c733d3e9295aef4b0ad
Pranav Chaudhary <pranavchaudhary2310@gmail.com>
2019-12-21 19:16:41 +0530

    Add Database.cpp

--- CodeMonk/Basics of Programming/Basics of Input Output/Database.cpp
+++ CodeMonk/Basics of Programming/Basics of Input Output/Database.cpp
@@ -4,6 +4,16 @@
 
 using namespace std;
 
+inline bool isInteger(const std::string &s) {
+	if (s.empty() || ((!isdigit(s[0])) && (s[0] != '-') && (s[0] != '+')))
+		return false;
+
+	char *p;
+	strtol(s.c_str(), &p, 10);
+
+	return (*p == 0);
+}
+
 int main(int argc, char const *argv[]) {
 	ios_base::sync_with_stdio(0);
 	cin.tie(nullptr);
@@ -16,6 +26,7 @@ int main(int argc, char const *argv[]) {
 		tuples++;
 		size_t max_size[attributes];
 		string database[tuples][attributes];
+		string type[attributes];
 
 		for (size_t i = 0; i < attributes; i++)
 			max_size[i] = numeric_limits<size_t>::min();
@@ -24,6 +35,18 @@ int main(int argc, char const *argv[]) {
 			for (size_t j = 0; j < attributes; j++) {
 				cin >> database[i][j];
 				max_size[j] = max(max_size[j], database[i][j].length());
+			}
+		}
+		for (size_t i = 0; i < attributes; i++) {
+			if (!database[tuples - 1][i].compare("true") or
+				!database[tuples - 1][i].compare("false")) {
+				type[i] = "bool";
+			} else if (isInteger(database[tuples - 1][i])) {
+				type[i] = "int";
+			} else if (database[tuples - 1][i].find('/') != string::npos) {
+				type[i] = "date";
+			} else {
+				type[i] = "string";
 			}
 		}
 
@@ -35,12 +58,12 @@ int main(int argc, char const *argv[]) {
 				cout << "+\n";
 			}
 			for (size_t j = 0; j < attributes; j++) {
-				if (j != attributes - 1) {
-					cout << "| " << setw(max_size[j] + 1) << setfill(' ')
-						 << left << database[i][j];
-				} else {
+				if (type[j] == "int" and i != 0) {
 					cout << "|" << setw(max_size[j] + 1) << setfill(' ')
 						 << right << database[i][j] << " ";
+				} else {
+					cout << "| " << setw(max_size[j] + 1) << setfill(' ')
+						 << left << database[i][j];
 				}
 			}
 			cout << "|" << endl;
