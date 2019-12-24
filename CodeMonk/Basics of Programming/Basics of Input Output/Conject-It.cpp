3639cd2beec365367dee498aa1244be5a6d63b80
Pranav Chaudhary <pranavchaudhary2310@gmail.com>
2019-12-24 20:18:34 +0530

    Add "Conject-It.cpp"

---
+++ CodeMonk/Basics of Programming/Basics of Input Output/Conject-It.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(nullptr);
+	int t;
+	cin >> t;
+	while (t--) {
+		cout << "YES\n";
+	}
+	return 0;
+}
